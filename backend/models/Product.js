f694c209e956c2309ccffa8a531eb9a926d4867d
saulscs <saulcoronasantos@ymail.com>
2019-05-14 14:52:29 -0500

    CD2 creacion del modelo de producto

---
+++ backend/models/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+
+const productSchema = new mongoose.Schema (
+{
+ title: String,
+ description: String,
+ price: String,
+ category: {
+   type: String,
+   enum:["Mac", "Iphone","Ipad","Apple Watch","Accesorios","Otros"]
+ },
+ image: String,
+ posted_by: {
+   type: Schema.Types.ObjectId,
+   ref: "User"
+ },
+ address:{
+  pais: String,
+  estado: String,
+  ciudad: String,
+  alcaldia: String,
+  colonia: String,
+  direccion: String,
+  location: { coords: [String], lat: String, lng: String }
+ },
+apply: {
+  email: String,
+  phone: String,
+},
+active: {
+  type: Boolean,
+  default: true
+}
+},
+{ 
+  timestamps: true, 
+  versionKey: false 
+}
+)
+
+
+
+module.exports = mongoose.model('Product', productSchema)
--- backend/models/User.js
+++ backend/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema (
     type: Boolean,
     default: true
   }
-},
+},   
 { 
   timestamps: true, 
   versionKey: false 
